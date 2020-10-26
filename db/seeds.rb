58e7ef5022f6c3edea5aeb6de6739a5ebf2660b5
Joel Rogalka <jrogalka@outlook.com>
2020-10-26 16:30:22 -0500

    Adds intial data pull from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
-# Loads data from csv file into usable data.
+require "csv"
+
+Book.delete_all
+Author.delete_all
+
+filename = Rails.root.join("db/books.csv")
+
+puts "Loading movies from #{filename}"
+
+csv_data = File.read(filename)
+books = CSV.parse(csv_data, headers: true, encoding: "utf-8", quote_char: "|")
+
+books.each do |b|
+  puts b["title"]
+end
