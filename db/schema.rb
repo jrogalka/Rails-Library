961f8de986f8dc11d953299281b072736a94945b
Joel Rogalka <jrogalka@outlook.com>
2020-10-27 18:14:41 -0500

    Migrates Subject and BookSubject tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_192610) do
+ActiveRecord::Schema.define(version: 2020_10_27_231348) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_subjects", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "subject_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_subjects_on_book_id"
+    t.index ["subject_id"], name: "index_book_subjects_on_subject_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -28,5 +37,13 @@ ActiveRecord::Schema.define(version: 2020_10_26_192610) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "book_subjects", "books"
+  add_foreign_key "book_subjects", "subjects"
   add_foreign_key "books", "authors"
 end
